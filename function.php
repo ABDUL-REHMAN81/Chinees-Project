2424182477119e2d3b099122b73484cedafe7805
ABDUL-REHMAN81 <50028289+ABDUL-REHMAN81@users.noreply.github.com>
2019-04-26 22:27:28 +0400

    Add files via upload

---
+++ function.php
@@ -0,0 +1,54 @@
+<?php
+
+//require_once('phpMailer/PHPMailerAutoload.php');
+if(isset($_REQUEST['email']))
+{
+    
+    $name= $_REQUEST['name'];
+    $lastname = $_REQUEST['lastname'];
+    $email = $_REQUEST['email'];
+    $pass = $_REQUEST['pass'];
+
+    $msg = '<table>
+    <tbody>
+       <tr>
+         <th>Name</th>
+         <td>'.$name.'</td>
+      </tr>
+       <tr>
+         <th>Lastname</th>
+          <td>'.$lastname.'</td>
+      </tr>
+       <tr>
+         <th>Email</th>
+          <td>'.$email.'</td>
+      </tr>
+       <tr>
+         <th>Password</th>
+          <td>'.$pass.'</td>
+      </tr>
+      
+    </tbody>
+      
+      
+    </table>';
+    
+    $subject = 'Registration Form Data';
+
+    $to      = 'abdulreehman121@gmail.com';
+    
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers .= 'From: abdulreehman121@gmail.com' . "\r\n" .
+    'Reply-To: abdulreehman121@gmail.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+    if(mail($to, $subject, $msg, $headers))
+    {
+      echo "Your details have been send successfuly";
+    }
+    else
+    {
+      echo "An error occur while sending your details";
+    }
+
+}
